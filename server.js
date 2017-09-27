d47cdaa5a10ab0b0f1189d92758df29fb65e84bb
rahmona-henry <rahmona.henry@icloud.com>
2017-09-27 22:17:01 +0100

    Add procfile

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const app = express()
 
 app.use(express.static('client'))
 
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.set('view engine','hbs')
+app.set('port', (process.env.PORT || 3000));
+
+// const env = process.env.NODE_ENV || 'development'
+
+///Routes/////
 app.get('/', function (req,res) {
   res.send(index.html)
 })
 
-app.listen(3000, function(){
-  console.log('Listening on port 3000')
+
+///Listen Route///////
+const port = process.env.PORT || 3000
+app.listen(port, function(){
+console.log('We have lift off on port ' + port)
 })
