6cd2a0c4a49378fb0fb28e3ec6e7a652a68e1b00
rahmona-henry <rahmona.henry@icloud.com>
2017-09-28 23:18:50 +0100

    final commit

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static('client'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','hbs')
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 4000));
+
 
 ///Get Routes/////
 app.get('/', function (req,res) {
@@ -15,11 +16,11 @@ app.get('/', function (req,res) {
 
 ///Post Routes/////
 app.post('https://forms.hubspot.com/uploads/form/v2/3939197/0a73a010-469c-4fcd-a45f-d3b02bcbaea7', function(req,res){
-    res.send('thankyou')
+    res.render('thankyou')
      })
 
 ///Listen Route///////
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 app.listen(port, function(){
 console.log('We have lift off on port ' + port)
 })
