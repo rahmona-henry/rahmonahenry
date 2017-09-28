6e8ca889d8723c74042b614afdb127988513e061
rahmona-henry <rahmona.henry@icloud.com>
2017-09-28 16:17:09 +0100

    hubspot api link

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(express.static('client'))
-
- app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 // app.set('view engine','hbs')
 app.set('port', (process.env.PORT || 3000));
 
@@ -17,12 +17,19 @@ app.get('/', function (req,res) {
 
 ///Post Routes/////
 app.post('https://forms.hubspot.com/uploads/form/v2/3939197/0a73a010-469c-4fcd-a45f-d3b02bcbaea7', function(req,res){
-      const firstName = req.body.firstName
-      const secondName = req.body.secondName
-      const email = req.body.email
-      res.send(firstName + ' ' + secondName + ' ' + email);
-
-})
+       console.log('this is the req.body',req.body)
+    //   .then(function(res){
+    //   //  console.log('this is the request',data)
+    //   // const firstName = req.body.firstName
+    //   // const secondName = req.body.secondName
+    //   // const email = req.body.email
+    //   //  res.send(firstName + ' ' + secondName + ' ' + email);
+    // })
+      // .then(function(error){
+      //   console.log('error')
+         res.redirect('/')
+      // })
+    })
 
 
 ///Listen Route///////
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('.btn').click(function(){
-  console.log('clicked')
+  console.log('clicked!')
 })
 })
