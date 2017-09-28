c0d9f0566dc64c307f5d31d8201a9eaa710321f0
rahmona-henry <rahmona.henry@icloud.com>
2017-09-28 22:02:34 +0100

    css

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const bodyParser = require('body-parser')
 app.use(express.static('client'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
- app.set('view engine','hbs')
+app.set('view engine','hbs')
 app.set('port', (process.env.PORT || 3000));
-// app.set('views',path.join(__dirname,'views'))
-
 
 ///Get Routes/////
 app.get('/', function (req,res) {
@@ -16,22 +14,9 @@ app.get('/', function (req,res) {
 })
 
 ///Post Routes/////
-app.post('/signUp', function(req,res){
-      //  console.log('this is the req.body',req.body)
-      //  .then(function(data){
-    //   //  console.log('this is the request',data)
-    //   // const firstName = req.body.firstName
-    //   // const secondName = req.body.secondName
-    //   // const email = req.body.email
-    //   //  res.send(firstName + ' ' + secondName + ' ' + email);
+app.post('https://forms.hubspot.com/uploads/form/v2/3939197/0a73a010-469c-4fcd-a45f-d3b02bcbaea7', function(req,res){
     res.render('thankyou')
      })
-    //    .catch(function(error){
-    //   //   console.log('error')
-    //      res.redirect('thankyou')
-    //    })
-    // })
-
 
 ///Listen Route///////
 const port = process.env.PORT || 3000
