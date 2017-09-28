b95bebe348c43fd0950bd19e8da210d09ded4d0a
rahmona-henry <rahmona.henry@icloud.com>
2017-09-28 21:48:28 +0100

    css

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser')
 app.use(express.static('client'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-// app.set('view engine','hbs')
+ app.set('view engine','hbs')
 app.set('port', (process.env.PORT || 3000));
-
+// app.set('views',path.join(__dirname,'views'))
 
 
 ///Get Routes/////
@@ -16,20 +16,21 @@ app.get('/', function (req,res) {
 })
 
 ///Post Routes/////
-app.post('https://forms.hubspot.com/uploads/form/v2/3939197/0a73a010-469c-4fcd-a45f-d3b02bcbaea7', function(req,res){
-       console.log('this is the req.body',req.body)
-    //   .then(function(res){
+app.post('/signUp', function(req,res){
+      //  console.log('this is the req.body',req.body)
+      //  .then(function(data){
     //   //  console.log('this is the request',data)
     //   // const firstName = req.body.firstName
     //   // const secondName = req.body.secondName
     //   // const email = req.body.email
     //   //  res.send(firstName + ' ' + secondName + ' ' + email);
+    res.render('thankyou')
+     })
+    //    .catch(function(error){
+    //   //   console.log('error')
+    //      res.redirect('thankyou')
+    //    })
     // })
-      // .then(function(error){
-      //   console.log('error')
-         res.redirect('/')
-      // })
-    })
 
 
 ///Listen Route///////
